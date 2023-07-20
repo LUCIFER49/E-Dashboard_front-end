71cceb84cbf4248e5f81fd70e3324261a4843441
LUCIFER49 <p1ush.prj@gmail.com>
2023-07-20 23:56:09 +0530

    fifth commit in frontend

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,21 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const ProductsList = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        getProducts();
-    },[]);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    const getProducts = () => {
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products");
+    result = await result.json();
+    setProducts(result);
+  }
+  console.warn("products", products);
 
-    }
-
-    return(
-        <div className=''>
-            <h1>Products List</h1>
-        </div>
-    );
-}
+  return (
+    <div className="product-list">
+      <h1>Products List</h1>
+      
+      {
+        products.map((item, index) => 
+        <ul>
+            <li>{ index+1 }</li>
+            <li>{ item.name}</li>
+            <li>$ { item.price }</li>
+            <li>{ item.category }</li>
+            <li>{ item.company}</li>
+        </ul>
+        )
+      }
+    </div>
+  );
+};
 
 export default ProductsList;
