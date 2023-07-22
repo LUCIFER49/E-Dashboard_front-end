ee725dbd3be08727f0517d0ee4d42a9f492a1d2c
LUCIFER49 <p1ush.prj@gmail.com>
2023-07-22 23:20:38 +0530

    sixth commit in frontend

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -11,24 +11,34 @@ const ProductsList = () => {
     let result = await fetch("http://localhost:5000/products");
     result = await result.json();
     setProducts(result);
+  };
+
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, )
   }
-  console.warn("products", products);
 
   return (
     <div className="product-list">
       <h1>Products List</h1>
-      
-      {
-        products.map((item, index) => 
+      <ul>
+        <li>Sr. No.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Company</li>
+        <li>Operation</li>
+      </ul>
+
+      {products.map((item, index) => (
         <ul>
-            <li>{ index+1 }</li>
-            <li>{ item.name}</li>
-            <li>$ { item.price }</li>
-            <li>{ item.category }</li>
-            <li>{ item.company}</li>
+          <li>{index + 1}</li>
+          <li>{item.name}</li>
+          <li>$ {item.price}</li>
+          <li>{item.category}</li>
+          <li>{item.company}</li>
+          <li><button onClick={() => deleteProduct(item._id)}>Delete</button></li>
         </ul>
-        )
-      }
+      ))}
     </div>
   );
 };
