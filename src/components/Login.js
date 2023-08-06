118ba9729188f99a0520be7bfef4a0637abe9e1a
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-06 16:09:26 +0530

    tenth commit regarding JWT integration

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,36 +7,49 @@ const Login = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const auth = localStorage.getItem('user');
-    if(auth) {
-      navigate('/');
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
     }
-  }, [])
+  }, []);
 
-  const handleLogin = async  () => {
+  const handleLogin = async () => {
     console.warn(email, password);
-    let result = await fetch('http://localhost:5000/login', {
-      method: 'POST',
+    let result = await fetch("http://localhost:5000/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     result = await result.json();
     console.warn(result);
-    if(result.name){
-      localStorage.setItem("user", JSON.stringify(result));
-      navigate('/');
-    }else {
-      alert('Please Enter correct Details');
+    if (result.auth) {
+      localStorage.setItem("user", JSON.stringify(result.user));
+      localStorage.setItem("token", JSON.stringify(result.auth));
+      navigate("/");
+    } else {
+      alert("Please Enter correct Details");
     }
-  }
+  };
 
   return (
     <div className="login">
       <h1>Login</h1>
-      <input type="text" className="inputField" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} value={email} />
-      <input type="password" className="inputField" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+      <input
+        type="text"
+        className="inputField"
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        className="inputField"
+        placeholder="Enter Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
       <button className="loginButton" type="button" onClick={handleLogin}>
         Login
       </button>
